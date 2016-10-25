71181e77fa757f87236b38e0d3f72444281498df
ric mclaughlin <ric@mclaughlin.today>
2016-10-24 17:12:33 -0700

    rework

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,26 @@
-var HtmlWebpackPlugin = require("html-webpack-plugin");
-var HTMLWebpackPluginConfig = new HtmlWebpackPlugin ({
-  template: __dirname + '/app/index.html',
-  filename: 'index.html',
-  inject: 'body'
-});
-
 module.exports = {
   entry: [
-    './app/index.js'
+    './src/index.js'
   ],
   output: {
-    path: __dirname + '/dist',
-    filename: 'index_bundle.js'
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
   },
   module: {
-    loaders: [
-      {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
-      {test: /\.css$/, loader: 'style-loader!css-loader'}
-    ]
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
+      }
+    }]
   },
-  plugins: [HTMLWebpackPluginConfig]
-}
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
+};
